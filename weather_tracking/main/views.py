667c2d3fcb4f964d0c6871b77aa27b62c9f07a2d
Irisha <irisha.kostevich@mail.ru>
2023-04-24 16:10:57 +0300

    Work with Api openweathermap.org

--- weather_tracking/main/views.py
+++ weather_tracking/main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+from .apikey import appid
+import requests
 def main_page(request):
-    return render(request, "main/main_page.html")
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    city = 'London'
+    res = requests.get(url.format(city)).json()
+    city_info = {"city": city, "temp": res["main"]["temp"], "icon": res["weather"][0]["icon"]}
+    context = {"info": city_info}
+    return render(request, "main/main_page.html", context)
