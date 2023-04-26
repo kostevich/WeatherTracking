f4e5fbcaca49b72e28b7e430dff0acb2f2920e15
Irisha <irisha.kostevich@mail.ru>
2023-04-26 17:54:07 +0300

    ReFACTORING

---
+++ weather_tracking/main/apikey.py
@@ -0,0 +1,2 @@
+# Secure storage of the api key.
+appid = 'c6d592e8ad0ebe73dffb7ea3897ec963'
--- weather_tracking/main/views.py
+++ weather_tracking/main/views.py
@@ -4,19 +4,33 @@ from .models import City
 import requests
 from .forms import CityForm
 def main_page(request):
+    # Делаем Api запрос к стороннему сайту.
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    # Если был выбран метод post.
     if request.method == 'POST':
+        # Формируем форму в которой отображается название города в виде строчки html.
         form = CityForm(request.POST)
+        # Сохранение значение города формы в базу данных.
         form.save()
-    form = CityForm
-    cities = City.objects.all()
 
+    # Формирование пустой формы.
+    form = CityForm
+    print(form)
+    # Переменная cities, отвечающая за вывод объектов класса City (Содержится только название города) .
+    cities = City.objects.all()
+    # Создаем  пустой список.
     all_cities = []
+    # Итерируем все объекты класса City (только название).
     for city in cities:
+        # Посылаем запрос к стороннему сайту, получаем в формате json.
         res = requests.get(url.format(city.name)).json()
+        # Формируем словарь с ключами: название, температура, иконка.
         city_info = {"city": city.name, "temp": res["main"]["temp"], "icon": res["weather"][0]["icon"]}
+        # Добавляем словарь в список с названиями городов, температурой и иконкой.
         all_cities.append(city_info)
+    # Формируем словарь значения которого: список с названиями городов, температурой и иконкой, и пустая форма.
     context = {"all_info": all_cities, "form": form}
+    # Возвращаем страницу html и нужные значения.
     return render(request, "main/main_page.html", context)
 
 
