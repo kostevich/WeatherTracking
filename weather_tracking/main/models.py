b46fbcfa93caefb2302412dd35b022d028cebf6d
Irisha <irisha.kostevich@mail.ru>
2023-05-14 21:36:56 +0300

    Update models.py

--- weather_tracking/main/models.py
+++ weather_tracking/main/models.py
@@ -1,28 +1,17 @@
 from django.db import models
 
-def validate_unique(value):
-    for city in City.objects.all():
-        if value == city.name:
-            City.objects.filter(name=city.name).delete()
-
-
-
-
-
 # Create model City for output cities.
 class City(models.Model):
     # Output of the name of the city with a length restriction.
-    name = models.CharField(max_length=30, validators=[validate_unique])
+    name = models.CharField(max_length=30, unique=True)
+    id = models.CharField(primary_key=True, unique=True)
     # Output str instead of the object.
     def __str__(self):
         # Output str of the name of the city instead of the object.
         return self.name
-
-class idopenweather(models.Model):
-    id = models.IntegerField(primary_key=True)
-
-    def __str__(self):
-        # Output str of the name of the city instead of the object.
         return self.id
+
+
+
 
 
