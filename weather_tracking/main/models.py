88c74cb66f2436178346b16e650b1a1aed9f1edb
Irisha <irisha.kostevich@mail.ru>
2023-05-16 18:08:07 +0300

    creating an id field to fill in

--- weather_tracking/main/models.py
+++ weather_tracking/main/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create model City for output cities.
 class City(models.Model):
     # Output of the name of the city with a length restriction.
-    name = models.CharField(max_length=30, unique=True)
-    id = models.CharField(primary_key=True, unique=True)
+    name = models.CharField(max_length=30)
+    id = models.CharField(primary_key=True, unique=True, max_length=20, default=0, editable=True)
     # Output str instead of the object.
     def __str__(self):
         # Output str of the name of the city instead of the object.
--- weather_tracking/main/views.py
+++ weather_tracking/main/views.py
@@ -8,6 +8,10 @@ from .forms import CityForm
 def main_page(request):
     # Делаем Api-запрос к стороннему сайту.
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    # Переменная cities, отвечающая за вывод объектов класса City (Содержится только название города).
+    cities = City.objects.all()
+    # Создаем  пустой список.
+    all_cities = []
     # Если был выбран метод post.
     if request.method == 'POST':
         # Формируем форму в которой отображается название города в виде строчки html.
@@ -15,13 +19,8 @@ def main_page(request):
         # Сохранение формы.
         form.save()
 
-
     # Формирование пустой формы.
     form = CityForm
-    # Переменная cities, отвечающая за вывод объектов класса City (Содержится только название города).
-    cities = City.objects.all()
-    # Создаем  пустой список.
-    all_cities = []
     # Итерируем все объекты класса City (только название).
     for city in cities:
         # Посылаем запрос к стороннему сайту, получаем в формате json.
@@ -32,6 +31,13 @@ def main_page(request):
         list_city_info.append(city_info)
         # Добавляем словарь в список с названиями городов, температурой и иконкой.
         all_cities.append(city_info)
+    try:
+        t = City.objects.get(id=0)
+        t.id = int(res["id"])
+        t.save()
+        City.objects.filter(id=0).delete()
+    except City.DoesNotExist:
+        print(0)
     # Формируем словарь значения которого: список с названиями городов, температурой и иконкой, и пустая форма.
     context = {"all_info": all_cities, "form": form, "list_city_info": list_city_info}
     # Возвращаем страницу html и нужные значения.
