b0691d4e33709608a800587785c875690237c852
Irisha <irisha.kostevich@mail.ru>
2023-05-17 15:50:16 +0300

    Update models.py

--- weather_tracking/main/models.py
+++ weather_tracking/main/models.py
@@ -5,12 +5,13 @@ class City(models.Model):
     # Output of the name of the city with a length restriction.
     name = models.CharField(max_length=30)
     id = models.CharField(primary_key=True, unique=True, max_length=20, default=0, editable=True)
+    temperature = models.CharField(max_length=10, default=0, editable=True)
+    icon = models.ImageField(default=0, editable=True)
     # Output str instead of the object.
     def __str__(self):
         # Output str of the name of the city instead of the object.
         return self.name
         return self.id
-
 
 
 
