2bc4d048f5e6a119a7ac35266bf41acc0caeefe2
Irisha <irisha.kostevich@mail.ru>
2023-05-26 17:20:14 +0300

    Update views.py

--- weather_tracking/main/views.py
+++ weather_tracking/main/views.py
@@ -10,6 +10,7 @@ def main_page(request):
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
     # Переменная cities, отвечающая за вывод объектов класса City (Содержится только название города).
     cities = City.objects.all()
+    re_name = None
     # Если был выбран метод post.
     if request.method == 'POST':
         # Формируем форму в которой отображается название города в виде строчки html.
@@ -17,6 +18,7 @@ def main_page(request):
         # Создать экземпляр формы из данных POST.
         form.save()
         res = requests.get(url.format(request.POST.get('name'))).json()
+        re_name = str(request.POST.get('name'))
         a = City.objects.get(id=0)
         a.id = int(res["id"])
         a.temperature = res["main"]["temp"]
@@ -27,10 +29,25 @@ def main_page(request):
         form.save()
     # Формирование пустой формы.
     form = CityForm()
+    for city in cities:
+        c = str(city)
+        if c == re_name:
+            t = city.temperature
+            i = city.icon
+            n = city.name
+
     # Формируем словарь значения которого: список с названиями городов, температурой и иконкой, и пустая форма.
-    context = {'cities': cities, "form": form}
+    context = {'cities': cities, "form": form, "re_name": re_name, "t": t, "n": n, "i": i}
     # Возвращаем страницу html и нужные значения.
     return render(request, "main/main_page.html", context)
+def edit_temperature(request):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    # Переменная cities, отвечающая за вывод объектов класса City (Содержится только название города).
+    cities = City.objects.all()
+    for city in cities:
+        edit_temperature = requests.get(url.format(city)).json()
+        City.objects.filter(name=city).update(temperature=edit_temperature["main"]["temp"])
+        print('r')
 
 
 
