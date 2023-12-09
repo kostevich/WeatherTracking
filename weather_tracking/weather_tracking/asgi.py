19284515c593ffe2246d5791cea3e22ffa1e6c35
Irisha <irisha.kostevich@mail.ru>
2023-12-09 13:36:25 +0300

    Refactoring

--- weather_tracking/weather_tracking/asgi.py
+++ weather_tracking/weather_tracking/asgi.py
@@ -1,15 +1,13 @@
-"""
-ASGI config for weather_tracking project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+#==========================================================================================#
+# >>>>> ПОДКЛЮЧЕНИЕ БИБЛИОТЕК И МОДУЛЕЙ <<<<< #
+#==========================================================================================#
 
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
+from django.core.asgi import get_asgi_application
+
 
 import os
 
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_tracking.settings')
 
--- weather_tracking/weather_tracking/wsgi.py
+++ weather_tracking/weather_tracking/wsgi.py
@@ -1,15 +1,12 @@
-"""
-WSGI config for weather_tracking project.
+#==========================================================================================#
+# >>>>> ПОДКЛЮЧЕНИЕ БИБЛИОТЕК И МОДУЛЕЙ <<<<< #
+#==========================================================================================#
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+from django.core.wsgi import get_wsgi_application
 
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
 
 import os
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_tracking.settings')
 
